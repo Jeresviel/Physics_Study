1f8b3bd177df77b78ee6279bd9a9c504ccf5cdaa
Jeresviel <deltajerrimy@qq.com>
2019-04-25 14:15:59 +0800

    add three-body problem and a book

--- computing_physics/maximum_likelihood_estimation.py
+++ computing_physics/maximum_likelihood_estimation.py
@@ -2,7 +2,6 @@ from scipy.optimize import fmin
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 x = [58.0,125.0,131.0,146.0,157.0,158.0,160.0,165.0,166.0,186.0,198.0,201.0,202.0,210.0,218.0]
 y = [173.0,334.0,311.0,344.0,317.0,416.0,337.0,393.0,400.0,423.0,510.0,442.0,504.0,479.0,533.0]
 sigma = [15.0,26.0,16.0,22.0,52.0,16.0,31.0,14.0,34.0,42.0,30.0,25.0,14.0,27.0,16.0]
@@ -14,12 +13,11 @@ def chi_square(ai,x,y,sigma):
 
 def mle(x,y,sigma):
     return fmin(chi_square,np.array([1,1]),args = (x,y,sigma))
-sum
 result = mle(x,y,sigma)
 chi_square_min = chi_square(result,x,y,sigma)
 plt.figure(1)
 plt.subplot(1,2,1)
-plt.title("最大似然估计")
+plt.title("MLE")
 x0 = np.arange(x[0],x[-1],0.05)
 y0 = result[0] * x0 + result[1]
 plt.errorbar(x,y,sigma,fmt = ".", elinewidth = 1, capsize = 2)
@@ -28,7 +26,7 @@ plt.ylabel('$y$', rotation = 360)
 plt.plot(x0, y0 , label = '$y=%.3fx+%.3f$' %(result[0],result[1]))
 plt.legend()
 plt.subplot(1,2,2)
-plt.title("参数误差")
+plt.title("Error")
 x1,y1 = np.linspace(2,2.5,500),np.linspace(5,70,500)
 X,Y = np.meshgrid(x1,y1)
 plt.contour(X,Y,chi_square((X,Y),x,y,sigma)-chi_square_min-1,0)
--- computing_physics/ordinary_differential_equation.py
+++ computing_physics/ordinary_differential_equation.py
@@ -5,7 +5,6 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def RK2(df,f0,area,h):#龙格-库塔2阶方法
     t = np.arange(area[0],area[1] + h,h)
-    h = h * np.ones(f0[0].shape)
     f = np.array([f0])
     for i in t[:-1]:
         f = np.append(f,[f[-1] + h * df(i + h/2,f[-1] + h/2 * df(i,f[-1]))],axis = 0)
@@ -13,7 +12,6 @@ def RK2(df,f0,area,h):#龙格-库塔2阶方法
 
 def RK4(df,f0,area,h):#龙格-库塔4阶方法方法
     t = np.arange(area[0],area[1] + h,h)
-    h = h * np.ones(f0[0].shape)
     f = np.array([f0])
     def slope(df,i,f,h):
         s1 = df(i,f[-1])
@@ -27,7 +25,6 @@ def RK4(df,f0,area,h):#龙格-库塔4阶方法方法
 
 def leap_frog(df,f0,area,h):
     t = np.arange(area[0],area[1]+h,h/2)
-    h = h * np.ones(f0[0].shape)
     f = np.append([f0],[f0 + h * df(t[0],f0)],axis = 0)
     for i in t[1:-1:2]:
         f = np.append(f,[f[-2] + h * df(i+h/2,f[-1])],axis = 0)
@@ -48,4 +45,45 @@ def Lorenz(t,p):
     dy = lambda t,p : r * p[0] - p[1] - p[0] * p[2]
     dz = lambda t,p : p[0] * p[1] - b * p[2]
     return np.array([dx(t,p),dy(t,p),dz(t,p)])
+
+def Lorentz(t,p):
+    m,q,E,B = 1,1,np.array([0,0,1]),np.array([0,0,1])
+    dr = lambda t,p : p[1]
+    def dv(t,p):
+        nonlocal m,q,E,B
+        return q/m * (E + np.cross(p[1],B))
+    return np.array([dr(t,p),dv(t,p)])
+
+def three_body_problem(t,p):
+    m1,m2,m3,G = 0*1.898E27,5.972E24,1.989E30,6.67408E-11
+    dr1 = lambda t,p : p[1]
+    def dv1(t,p):
+        nonlocal m1,m2,m3,G
+        return G*m2/(np.linalg.norm(p[2]-p[0])**3)*(p[2]-p[0])+G*m3/(np.linalg.norm(p[4]-p[0])**3)*(p[4]-p[0])
+    dr2 = lambda t,p : p[3]
+    def dv2(t,p):
+        nonlocal m1,m2,m3,G
+        return G*m3/(np.linalg.norm(p[4]-p[2])**3)*(p[4]-p[2])+G*m1/(np.linalg.norm(p[0]-p[2])**3)*(p[0]-p[2])
+    dr3 = lambda t,p : p[5]
+    def dv3(t,p):
+        nonlocal m1,m2,m3,G
+        return G*m1/(np.linalg.norm(p[0]-p[4])**3)*(p[0]-p[4])+G*m2/(np.linalg.norm(p[2]-p[4])**3)*(p[2]-p[4])
+    return np.array([dr1(t,p),dv1(t,p),dr2(t,p),dv2(t,p),dr3(t,p),dv3(t,p)])
 #对应的微分方程定义，输入p为矢量，输出也是一个矢量（或矩阵），p中变量顺序和输出是对应的
+t1,f1 = RK4(three_body_problem,np.array([[7.78E11,0,0],[0,1.307E4,0],[1.4959787E11,0,0],[0,2.98E4,0],[0,0,0],[0,0,0]]),[0,1E9],1E6)
+t2,f2 = RK4(Lorentz,np.array([[0,0,0],[0,1,0]]),(0,20),0.1)
+plt.figure()
+plt.plot(f1[:,0,0],f1[:,0,1],label="$Jupiter$")
+plt.plot(f1[:,2,0],f1[:,2,1],label="$Earth$")
+plt.plot(f1[:,4,0],f1[:,4,1],label="$the Sun$")
+plt.legend()
+fig = plt.figure()
+ax = fig.gca(projection = '3d')
+x = f2[:,0,0]
+y = f2[:,0,1]
+z = f2[:,0,2]
+plt.plot(x,y,z,label="Orbit of A Charged Particle")
+plt.xlabel("$x$")
+plt.ylabel("$y$")
+plt.legend()
+plt.show()
